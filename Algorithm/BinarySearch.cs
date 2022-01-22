b496e87af068a9a8a46e75e196b4e5f37794f672
wag_k <captenkenta@gmail.com>
2022-01-22 17:27:42 +0900

    探索系のアルゴリズムはひとまずここに書く FirstCommit

---
+++ Algorithm/BinarySearch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// めぐる式２文探索
+    /// [【めぐるのアルゴリズム講座】二分探索（整数）の書き方 難しさ：４](https://twitter.com/meguru_comp/status/697008509376835584)
+    /// left は常に条件を満たさず、right は常に条件を満たす
+    /// </summary>
+    public static class BinarySearch<T>
+    where T : IComparable
+    {
+        /// <summary>
+        /// Delegate of criteia function
+        /// Check wheter the index satisfy the condition
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public delegate bool IsOK(IList<T> elements, int index, T key);
+
+        /// <summary>
+        /// Run binary search with default check method
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <param name="index"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int Search(IList<T> elements, T key){
+            return Search(elements, key, DefaultIsOK);
+        }
+
+        /// <summary>
+        /// Run binary search with designated check method
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <param name="index"></param>
+        /// <param name="key"></param>
+        /// <param name="isOK">Check method</param>
+        /// <returns></returns>
+        public static int Search(IList<T> elements, T key, IsOK isOK){
+            int ng = -1;
+            int ok = elements.Count;
+
+            while(Math.Abs(ok-ng) > 1){
+                int mid = (ok + ng) / 2;
+                if(isOK(elements, mid, key)){
+                    ok = mid;
+                } else { 
+                    ng = mid;
+                }
+                
+            }
+            return ok;
+        }
+
+        /// <summary>
+        /// Default typical binary search condition check method
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool DefaultIsOK(IList<T> elements, int index, T key)
+        {
+            return elements[index].CompareTo(key) >= 0;
+        }
+    }
+}
---
+++ AlgorithmTests/BinarySearchTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class BinarySearchTests
+    {
+        static int RandCounter { get; set; }
+        
+        [TestMethod()]
+        public void SearchTest_Int()
+        {
+            int totalTestCnt = 100;
+            var rand = new Random(DateTime.Now.GetHashCode()+RandCounter);
+            for (int testCnt = 1; testCnt <= totalTestCnt; ++testCnt){
+                // 様々な大きさの配列でテスト
+                var testCase = (
+                    from index in Enumerable.Range(0, testCnt)
+                    let num = rand.Next()
+                    orderby num
+                    select num
+                ).ToArray();
+                var testKey = rand.Next();
+                
+                var expected = testCase.Length;
+                for (int index = 0; index < testCase.Length; ++index){
+                    if(BinarySearch<int>.DefaultIsOK(testCase, index, testKey)) {
+                        expected = index;
+                        break;
+                    }
+                }
+                Debug.WriteLine($"TestCase: {testCnt}, Key: {testKey}");
+                Debug.WriteLine(String.Join(",", testCase));
+
+                var actual = BinarySearch<int>.Search(testCase, testKey);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod()]
+        public void SearchTest_Int2()
+        {
+            // 様々な大きさの配列でテスト
+            var testCase = new int[]{2, 2, 2, 3};
+            
+            var actual1 = BinarySearch<int>.Search(testCase, 1);
+            var actual2 = BinarySearch<int>.Search(testCase, 2);
+            var actual3 = BinarySearch<int>.Search(testCase, 3);
+            var actual4 = BinarySearch<int>.Search(testCase, 4);
+            Assert.AreEqual(0, actual1);
+            Assert.AreEqual(0, actual2);
+            Assert.AreEqual(3, actual3);
+            Assert.AreEqual(4, actual4);
+        }
+        
+        [TestMethod()]
+        public void SearchTest_Double()
+        {
+            int totalTestCnt = 100;
+            var rand = new Random(DateTime.Now.GetHashCode()+RandCounter);
+            for (int testCnt = 1; testCnt <= totalTestCnt; ++testCnt){
+                // 様々な大きさの配列でテスト
+                var testCase = (
+                    from index in Enumerable.Range(0, testCnt)
+                    let num = rand.NextDouble()
+                    orderby num
+                    select num
+                ).ToArray();
+                var testKey = rand.Next();
+                
+                var expected = testCase.Length;
+                for (int index = 0; index < testCase.Length; ++index){
+                    if(BinarySearch<double>.DefaultIsOK(testCase, index, testKey)) {
+                        expected = index;
+                        break;
+                    }
+                }
+                Debug.WriteLine($"TestCase: {testCnt}, Key: {testKey}");
+                Debug.WriteLine(String.Join(",", testCase));
+
+                var actual = BinarySearch<double>.Search(testCase, testKey);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+        
+    }
+}
